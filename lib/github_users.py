2bb0e813c7bba3a823046122ed9a3e2c8d2f448d
denmat <tu2Bgone@gmail.com>
2018-06-11 22:05:30 +1000

    Change class to use instance methods and move output manipulation out

--- lib/github_users.py
+++ lib/github_users.py
@@ -9,45 +9,32 @@ class GithubUsers():
         self.org = org
         self.team = team
 
-    @staticmethod
-    def _g():
+    def _g(self):
         return Github(Configuration.github_auth_key())
 
-    @staticmethod
     def _validate_org(g, org_name):
         name = g.get_organization(org_name).name
         return name
 
-    @staticmethod
-    def _get_team_id(g, org_name, team_name):
+    def _get_team_id(self, g, org_name, team_name):
         teams = (team for team in g.get_organization(org_name).get_teams() if (team.name == team_name))
         for team in teams:
             return team.id
 
-    @staticmethod
-    def _team_members(g, org_name, team_id):
+    def _team_members(self, g, org_name, team_id):
         return g.get_organization(org_name).get_team(team_id)
 
-    @staticmethod
-    def _members_logins(members):
+    def _members_logins(self, members):
         for members in members.get_members():
             yield members.login
 
-    @staticmethod
-    def _get_public_keys(g, login):
+    def _get_public_keys(self, g, login):
         for key in g.get_user(login).get_keys():
             return key.key
 
-    @staticmethod
-    def _shorten_key(key):
-        if not key:
-            return 'None'
-        start, end = key[:16], key[-8:]
-        return start + '....' + end
-
-    def list_users(self, org, team):
+    def _list_users(self, org, team):
         data = []
-        team_id = self._get_team_id(self._g(), org, team)
+        team_id = self._get_team_id(self._g(), self.org, self.team)
         members = self._team_members(self._g(), org, team_id)
         for member in self._members_logins(members):
             key = self._get_public_keys(self._g(), member)
@@ -55,5 +42,9 @@ class GithubUsers():
                 present = 'Present'
             else:
                 present = 'Not Present'
-            data.append([member, present, self._shorten_key(key)])
+            data.append([member, present, key])
+        return data
+
+    def list_users(self):
+        data =  self._list_users(self.org, self.team)
         return data
